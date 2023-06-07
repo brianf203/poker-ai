fec17a472542583a441cb4a0662144ae274f3e2c
brianf203 <briancf203@gmail.com>
2023-06-07 16:42:22 -0700

    fixed formatting

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,65 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './Game.css';
 import image1 from '../pictures/2c.png';
+import image2 from '../pictures/2d.png';
+import image3 from '../pictures/2h.png';
+import image4 from '../pictures/2s.png';
+
 
 const Game = () => {
-    
-    const [sliderValue, setSliderValue] = useState(50);
+
+    const [sliderValue, setSliderValue] = useState(100);
+    const [buttonMessage, setButtonMessage] = useState('');
+    const [currentPot, setCurrentPot] = useState(0);
+    const prevButtonMessageRef = useRef('');
+    const [userBal, setUserBal] = useState(10000);
+    const [AIBal, setAIBal] = useState(10000);
 
     const handleSliderChange = (event) => {
         setSliderValue(event.target.value);
     };
 
     const handleCheckButtonClick = () => {
-        alert(`Check / Fold`);
+        setButtonMessage('You checked');
     };
 
     const handleBetButtonClick = () => {
-        alert(`Bet ${sliderValue}`);
+        setButtonMessage(`You bet ${sliderValue}`);
+        setCurrentPot((currentPot) => currentPot + parseInt(sliderValue));
+        setUserBal((userBal) => userBal - parseInt(sliderValue));
     };
+
+    useEffect(() => {
+        prevButtonMessageRef.current = buttonMessage;
+      }, [buttonMessage]);
 
     return (
         <div className="game-container">
+            <div className="top-left">Brian's AI: {AIBal.toLocaleString()}</div>
+            <div className="top-right">
+                {buttonMessage}
+            </div>
+            <div className="middle-right">
+                <div className="current-pot">
+                    Current pot: {currentPot}
+                </div>
+            </div>
             <div className="top-middle">
                 <img src={image1} alt={image1} style={{ width: '25%', height: 'auto' }} />
-                <img src={image1} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
+                <img src={image3} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
             </div>
             <div className="middle-middle">
                 <img src={image1} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
+                <img src={image3} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
+                <img src={image4} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
                 <img src={image1} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
-                <img src={image1} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
-                <img src={image1} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
-                <img src={image1} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
+                <img src={image2} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
             </div>
             <div className="left-middle">
-                <img src={image1} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
+                <img src={image4} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
             </div>
             <div className="bottom-middle">
-                <img src={image1} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
-                <img src={image1} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
+                <img src={image2} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
+                <img src={image3} alt="Image 1" style={{ width: '25%', height: 'auto' }} />
             </div>
+            <div className="bottom-left">You: {userBal.toLocaleString()}</div>
             <div className="bottom-right">
-                <div className="input-container">
                     <div className="slider-container">
                         <input
                             type="text"
-                            placeholder="Enter text"
+                            placeholder="Enter bet"
                             className="text-input"
                             value={`${sliderValue}`}
                             readOnly
                         />
                         <input
                             type="range"
-                            min="0"
-                            max="100"
+                            min="100"
+                            max={userBal}
                             value={sliderValue}
                             className="slider-input"
                             onChange={handleSliderChange}
                         />
                     </div>
-                </div>
                 <div className="buttons-container">
-                    <button className="check-fold-button" onClick={handleCheckButtonClick}>Check / Fold</button>
+                    <button className="check-fold-button" onClick={handleCheckButtonClick}>Check </button>
                     <button className="bet-button" onClick={handleBetButtonClick}>Bet {sliderValue}</button>
                 </div>
             </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
                 <div className="text-container">
                     <h1>Play against the best <span style={{ color: 'rgb(58, 175, 170)' }}>poker AI</span> in the world</h1>
                     <p>Challenge the best poker AI in the world. Brian's AI has proven its dominance against top AI agents and professional players over thousands of hands with an impressive win rate of 23BB/100.</p>
-                    <p>Dive into the realm of strategic thinking and master poker strategy. Analyzing vast data with sophisticated mathematical models, our nengine employs probability theory to assess odds perfectly. Witness the power of AI-driven probability decision-making with Brian's AI.</p>
+                    <p>Dive into the realm of strategic thinking and master poker strategy. Analyzing vast data with sophisticated mathematical models, our engine employs probability theory to assess odds perfectly. Witness the power of AI-driven probability decision-making with Brian's AI.</p>
                     <button className="start-game-button">
                         <Link to="/game" style={{ textDecoration: 'none', color: 'white' }}>Start Game</Link>
                     </button>
